5ad7d9e018d19ef28b14b85bfb020ce7997c22ab
M Zubair <md.zubair143598@gmail.com>
2022-12-06 18:09:33 +0500

    error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import './App.scss';
 import Random from "./Components/random";
-import PostionPra from './Components/PostionPra';
+// import PostionPra from './Components/PostionPra';
 
 function App() {
   return (
